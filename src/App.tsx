178899e143b111aa8ebc7d30417aae57600b530d
shukuravazov <shukuravazov7@gmail.com>
2022-12-12 00:11:28 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="mb-4">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,48 @@
+import { Button, Container, Nav, Navbar as NavBarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
+// import {Fa} from '@fortawesome/free-solid-svg-icons'
+
+function NavBar() {
+  return (
+    <NavBarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to={"/"} as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to={"/store"} as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to={"/about"} as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          style={{ width: "5rem", height: "3rem" }}
+          variant="outline-primary"
+        >
+          <FontAwesomeIcon icon={faCartShopping} />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 10,
+              right: 10,
+              transform: "translate(40%, 40%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavBarBs>
+  );
+}
+
+export default NavBar;

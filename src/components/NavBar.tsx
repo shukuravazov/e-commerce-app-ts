3aedc1015723d4821bb050a648f854f3949dcaf9
shukuravazov <shukuravazov7@gmail.com>
2022-12-13 03:19:06 +0100

    Navbar cart counter implemented

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,10 @@ import { Button, Container, Nav, Navbar as NavBarBs } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-
-// import {Fa} from '@fortawesome/free-solid-svg-icons'
+import { useShoppingCart } from "../context /ShoppingCartContext";
 
 function NavBar() {
+  const { openCart, cartQuantit } = useShoppingCart();
   return (
     <NavBarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
@@ -20,26 +20,29 @@ function NavBar() {
             About
           </Nav.Link>
         </Nav>
-        <Button
-          style={{ width: "5rem", height: "3rem" }}
-          variant="outline-primary"
-        >
-          <FontAwesomeIcon icon={faCartShopping} />
-          <div
-            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
-            style={{
-              color: "white",
-              width: "1.5rem",
-              height: "1.5rem",
-              position: "absolute",
-              bottom: 10,
-              right: 10,
-              transform: "translate(40%, 40%)",
-            }}
+        {cartQuantit > 0 && (
+          <Button
+            onClick={openCart}
+            style={{ width: "5rem", height: "3rem" }}
+            variant="outline-primary"
           >
-            3
-          </div>
-        </Button>
+            <FontAwesomeIcon icon={faCartShopping} />
+            <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 10,
+                right: 10,
+                transform: "translate(40%, 40%)",
+              }}
+            >
+              {cartQuantit}
+            </div>
+          </Button>
+        )}
       </Container>
     </NavBarBs>
   );

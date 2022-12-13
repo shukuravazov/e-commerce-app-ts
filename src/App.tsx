d38c69b96cb3fef9fe94d81ca08b2f7eeb17d4ea
shukuravazov <shukuravazov7@gmail.com>
2022-12-13 03:04:52 +0100

    context component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { ShoppingCartProvider } from "./context /ShoppingCartContext";
 import NavBar from "./components/NavBar";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -7,7 +8,7 @@ import Store from "./pages/Store";
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Container className="mb-4">
         <NavBar />
         <Routes>
@@ -16,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 }
 
